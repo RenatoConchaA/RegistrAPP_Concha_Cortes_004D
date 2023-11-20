0f79771d7ec3aecdc63ffe88f29000a666e07226
Renato Concha <ren.concha@duocuc.cl>
2023-11-20 02:39:21 -0300

    Experiencia 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './servicios/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public authService: AuthService) {}
+
+  opcionesalumno=[
+    {
+      name:'Login',
+      icon:'log-in',
+      redirecTo:'/login'
+    },
+    {
+      name:'Registro',
+      icon:'create',
+      redirecTo:'/select-registro'
+    },
+    {
+      name:'Información',
+      icon:'information',
+      redirecTo:'/informacionalumno'
+    },
+    {
+      name:'Scannear QR',
+      icon:'scan',
+      redirecTo:'/scanearqr'
+    },
+    {
+      name:'Perfil',
+      icon:'person',
+      redirecTo:'/perfilalumno'
+    }
+  ];
+    opcionesdocente=[
+    {
+      name:'Login',
+      icon:'log-in',
+      redirecTo:'/login'
+    },
+    {
+      name:'Registro',
+      icon:'create',
+      redirecTo:'/select-registro'
+    },
+    {
+      name:'Información',
+      icon:'information',
+      redirecTo:'/informacion'
+    },
+    {
+      name:'Generar QR',
+      icon:'qr-code-outline',
+      redirecTo:'/qr'
+    },
+    {
+      name:'Perfil',
+      icon:'person',
+      redirecTo:'/perfil'
+    }
+  ];
+  
+
+  get menuItems(){
+    return this.authService.usertype==='alumno'? this.opcionesalumno :this.opcionesdocente
+  }
 }
